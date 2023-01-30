7ca270b36e18f11de2489f687d16012ec825ce63
tarunnagle04 <tarunnagle04@gmail.com>
2023-01-30 16:52:05 +0530

    m

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import FunctionalUserForm from './components/Form/FunctionalUserForm';
+import { Routes, Route } from 'react-router-dom';
+import { UserTile } from './components/Form/UserTile';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+          <Route path="/" element={<FunctionalUserForm/>}/>  
+          <Route path="/userTile" element={<UserTile/>}/>
+      </Routes>
     </div>
   );
 }
